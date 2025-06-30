b78240c3e2b97ecfe8517bd5bcaddf6502c2f276
youzi-3222 <youzi_2333@163.com>
2025-06-30 16:04:27 +0800

    test(FlightInfo): 为 FlightInfo 类添加测试

---
+++ tests/atc/core/scene/aircraft/test_flight_info.py
@@ -0,0 +1,106 @@
+import pytest
+
+from src.atc.core.scene.aircraft.flight_info import FlightInfo
+from src.atc.core.scene.aircraft.wake import Wake
+
+
+@pytest.mark.parametrize(
+    "kwargs,expected",
+    [
+        (
+            {
+                "ssr_code": 3427,
+                "airline": "CZ",
+                "raw_flight_no": "328",
+                "arrival": True,
+                "rwy": "20R",
+                "aircraft_type": "A380",
+                "wake": Wake.HEAVY,
+            },
+            "CZ328",
+        ),
+        (
+            {
+                "ssr_code": 7700,
+                "airline": "AI",
+                "raw_flight_no": "171",
+                "arrival": False,
+                "rwy": "23",
+                "aircraft_type": "B789",
+                "wake": Wake.HEAVY,
+            },
+            "AI171",
+        ),
+    ],
+)
+def test_flight_info_flight_no(kwargs, expected):
+    flight_info = FlightInfo(**kwargs)
+    assert flight_info.flight_no == expected
+
+
+@pytest.mark.parametrize(
+    "kwargs,expected",
+    [
+        (
+            {
+                "ssr_code": 3427,
+                "airline": "CZ",
+                "raw_flight_no": "328",
+                "arrival": True,
+                "rwy": "20R",
+                "aircraft_type": "A380",
+                "wake": Wake.HEAVY,
+            },
+            "南航",
+        ),
+        (
+            {
+                "ssr_code": 7700,
+                "airline": "AI",
+                "raw_flight_no": "171",
+                "arrival": False,
+                "rwy": "23",
+                "aircraft_type": "B789",
+                "wake": Wake.HEAVY,
+            },
+            "Air India",
+        ),
+    ],
+)
+def test_flight_info_airline_callsign(kwargs, expected):
+    flight_info = FlightInfo(**kwargs)
+    assert flight_info.airline_callsign == expected
+
+
+@pytest.mark.parametrize(
+    "kwargs,expected",
+    [
+        (
+            {
+                "ssr_code": "3427",
+                "airline": "CZ",
+                "raw_flight_no": "328",
+                "arrival": True,
+                "rwy": "20R",
+                "aircraft_type": "A380",
+                "wake": Wake.HEAVY,
+            },
+            True,
+        ),
+        (
+            {
+                "ssr_code": "7700",
+                "airline": "AI",
+                "raw_flight_no": "171",
+                "arrival": False,
+                "rwy": "23",
+                "aircraft_type": "B789",
+                "wake": Wake.HEAVY,
+            },
+            False,
+        ),
+    ],
+)
+def test_flight_info_chinese(kwargs, expected):
+    flight_info = FlightInfo(**kwargs)
+    assert flight_info.chinese == expected
